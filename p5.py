a19a0c26803a7b4da99dbdbc489024c0f6b62119
vvmv9199 <37921553+vvmv9199@users.noreply.github.com>
2018-03-30 09:51:02 +0530

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,7 @@
+length=3.5;
+breadth=4.8;
+area=length*breadth;
+print("length=",length);
+print("breadth=",breadth);
+print("Area=",area);
+
---
+++ p9.py
@@ -0,0 +1,4 @@
+nm="USA";
+b='$';
+print("country name is",nm);
+print("currency symbol of",nm "is",ab); 
